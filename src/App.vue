4614803f1f434e75d89e3397bb5fc1bb6a768991
kanonsion <2273091645@qq.com>
2021-10-28 10:00:18 +0800

    feat: 删除无用代码

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <template>
     <div id="app">
-        <hello-world></hello-world>
     </div>
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld.vue'
-
     export default {
         name: 'app',
-        components: {
-            HelloWorld
-        }
     }
 </script>
 
